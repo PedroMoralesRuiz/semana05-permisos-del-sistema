56440b7c7df5a8b9904c4079d356e5a49762d8b2
Pedro <centro.formaciondigital@gmail.com>
2018-02-10 18:57:55 -0500

    clase de androi semana 05

---
+++ app/src/main/java/com/example/juan/permisosdelsistma/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.juan.permisosdelsistma;
+
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(validarpermisos())
+        {
+            iniciarApp();
+        }
+            else
+        {
+            solicitarPermisos();
+        }
+    }
+
+    private void solicitarPermisos() {
+
+        LinearLayout p = findViewById(R.id.mensaje2);
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,CAMERA) ||
+                ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,WRITE_EXTERNAL_STORAGE) ||
+                ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,READ_EXTERNAL_STORAGE) ||
+                ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,ACCESS_FINE_LOCATION) ||
+                ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, ACCESS_COARSE_LOCATION))
+        {
+            Snackbar.make(p,"solicitar nuevamente los permisos del sistema.", Snackbar.LENGTH_INDEFINITE).setAction(android.R.string.ok, new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+
+                            CAMERA,
+                            WRITE_EXTERNAL_STORAGE,
+                            READ_EXTERNAL_STORAGE,
+                            ACCESS_COARSE_LOCATION,
+                            ACCESS_FINE_LOCATION
+
+                    }, 50
+                    );
+                }
+            }).show();
+        }
+        else{
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+
+                            CAMERA,
+                            WRITE_EXTERNAL_STORAGE,
+                            READ_EXTERNAL_STORAGE,
+                            ACCESS_COARSE_LOCATION,
+                            ACCESS_FINE_LOCATION
+
+                    }, 50
+            );
+        }
+    }
+
+    private void iniciarApp() {
+        Toast.makeText(getApplicationContext(),"La aplicación ya puede iniciar correctamente", Toast.LENGTH_LONG).show();
+    }
+
+    private boolean validarpermisos() {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
+        {
+            return true;
+        }
+        if(ContextCompat.checkSelfPermission(MainActivity.this,CAMERA) == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(MainActivity.this,WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(MainActivity.this,READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+        ContextCompat.checkSelfPermission(MainActivity.this,ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+        ContextCompat.checkSelfPermission(MainActivity.this,ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+
+            return true;
+        }
+        return false;
+    }
+}
